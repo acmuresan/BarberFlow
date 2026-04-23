db10b12bd000f29fe175a4baab9674f1b63ec04b
marta051 <navarrolozano@outlook.com>
2026-04-24 01:31:15 +0200

    Update servicios.service.spec.ts

--- frontend/src/app/core/services/servicios.service.spec.ts
+++ frontend/src/app/core/services/servicios.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiciosService } from './servicios.service.';
+import { ServiciosService } from './servicios.service';
 
 describe('Servicios', () => {
   let service: ServiciosService;
