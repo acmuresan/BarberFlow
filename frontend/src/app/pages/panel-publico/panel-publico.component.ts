3933a8047a08a2772b8707b4aaf54877647bc300
marta051 <navarrolozano@outlook.com>
2026-04-25 02:07:08 +0200

    Panel publico

---
+++ frontend/src/app/pages/panel-publico/panel-publico.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-publico',
+  imports: [],
+  templateUrl: './panel-publico.html',
+  styleUrl: './panel-publico.css',
+})
+export class PanelPublicoComponent {}
