23d89869e723267495767a5ac1cd45aef6488478
marta051 <navarrolozano@outlook.com>
2026-05-07 17:28:48 +0200

    Backend borrado

--- backend/src/models/Token.ts
+++
@@ -1,8 +0,0 @@
-import { Rol } from "./Usuario.js";
-
-export interface TokenPayload {
-  usuario_id: number;
-  rol: Rol;
-  email: string;
-  barbero_id: number | null;
-}
--- backend/src/models/WalkIn.ts
+++
@@ -1,8 +0,0 @@
-export interface Walkin {
-  id: number;
-  nombre: string;
-  barberos_id: number | null;
-  hora_llegada: Date;
-  estado: "esperando" | "atendiendo" | "completado" | "cancelado";
-  created_at: Date;
-}
